be7d857384371ac4a2a66d97e5b43e0613a78745
Gizmato <qaisnotacrime@gmail.com>
2017-06-29 21:34:31 +0300

    First test has been added

---
+++ sel8-first/sel8-first/CSharpSeleniumTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Remote;
+
+namespace sel8_first
+{
+    [TestFixture]
+    public class CSharpSeleniumTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            InternetExplorerOptions IeCapabilities = new InternetExplorerOptions();
+            IeCapabilities.IgnoreZoomLevel = true;
+            IeCapabilities.EnableNativeEvents = false;
+            IeCapabilities.EnablePersistentHover = true;
+            driver = new InternetExplorerDriver(IeCapabilities);
+            
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(50));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://www.google.com/";
+            wait.Until(ExpectedConditions.TitleContains("Google"));
+            driver.FindElement(By.CssSelector("div.logo-subtext"));
+            driver.FindElement(By.Name("q")).SendKeys("webdriver");
+            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("div.logocont > h1 > a > img")));
+            driver.FindElement(By.CssSelector("button.sbico-c")).Click();
+            wait.Until(ExpectedConditions.TitleIs("webdriver - Поиск в Google"));
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
---
+++ sel8-first/sel8-first/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("sel8-first")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sel8-first")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("02af766c-f8d6-4918-807a-9a504480e16d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
